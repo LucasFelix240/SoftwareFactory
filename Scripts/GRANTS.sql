7fbd90853ec8781d31cffc6db852100e08f9de81
LucasFelix240 <lucas.felix.et12@gmail.com>
2022-11-23 14:02:00 -0300

    Última corrección de todos los errores

--- Scripts/GRANTS.sql
+++ Scripts/GRANTS.sql
@@ -1,20 +1,16 @@
+-- Active: 1646654372192@@127.0.0.1@3306@softwarefactory
 USE SoftwareFactory;
-DELIMITER $$
+
 CREATE USER IF NOT EXISTS 'Administrador'@'%' IDENTIFIED BY 'passAdministrador';
 GRANT SELECT ON SoftwareFactory.* TO 'Administrador'@'%';
 GRANT UPDATE ON SoftwareFactory.Proyecto TO 'Administrador'@'%';
-END $$
 
-DELIMITER $$
 CREATE USER IF NOT EXISTS 'PM'@'10.3.45.%' IDENTIFIED BY 'passPM';
 GRANT SELECT ON SoftwareFactory.* TO 'PM'@'10.3.45.%';
 GRANT INSERT, UPDATE(calificacion) ON SoftwareFactory.Experiencia TO 'PM'@'10.3.45.%';
 GRANT INSERT ON SoftwareFactory.Empleado TO 'PM'@'10.3.45.%';
 GRANT INSERT ON SoftwareFactory.Requerimiento TO 'PM'@'10.3.45.%';
 GRANT INSERT, UPDATE(fin) ON SoftwareFactory.Tarea TO 'PM'@'10.3.45.%';
-END $$
 
-DELIMITER $$
 CREATE USER IF NOT EXISTS 'Empleado'@'10.3.45.%' IDENTIFIED BY 'passEmpleado';
 GRANT SELECT ON SoftwareFactory.* TO 'Empleado'@'10.3.45.%';
-END $$
--- Scripts/TRIGGERS.sql
+++ Scripts/TRIGGERS.sql
@@ -1,11 +1,24 @@
+-- Active: 1646654372192@@127.0.0.1@3306@softwarefactory
 USE SoftwareFactory;
 DELIMITER $$
 CREATE TRIGGER BeforeInsertTarea BEFORE INSERT ON Tarea
 FOR EACH ROW
 BEGIN
-        IF(NEW.calificacion < OLD.complejidad) THEN
+        DECLARE varCalificacion TINYINT;
+        DECLARE varComplejidad TINYINT;
+
+        SELECT calificacion INTO varCalificacion
+        FROM Experiencia
+        WHERE cuil = NEW.cuil;
+
+        SELECT complejidad INTO varComplejidad
+        FROM Requerimiento
+        WHERE idRequerimiento = NEW.idRequerimiento;
+
+        IF(varCalificacion < varComplejidad) THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Calificación insuficiente';
+        SET MESSAGE_TEXT = 'Calificacion insuficiente';
+        END IF;
 END $$
 
 DELIMITER $$
@@ -13,5 +26,5 @@ CREATE TRIGGER AftInsEmpleado AFTER INSERT ON Empleado
 FOR EACH ROW
 BEGIN
         INSERT INTO Experiencia (cuil, idTecnologia, calificacion)
-                        VALUES  (NEW.cuil, NEW.idTecnologia, 0);
+                        VALUES  (NEW.cuil, idTecnologia, 0);
 END $$
