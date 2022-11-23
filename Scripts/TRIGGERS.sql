973c503434a5a9454149c42ea149284a1eefdbc5
LucasFelix240 <lucas.felix.et12@gmail.com>
2022-11-23 15:48:39 -0300

    Agregación del Insert para fallo de Trigger

--- Scripts/TRIGGERS.sql
+++ Scripts/TRIGGERS.sql
@@ -36,3 +36,23 @@ BEGIN
 END $$
 
 -- Trigger para que falle a proposito
+INSERT INTO Empleado (cuil, nombre, apellido, contratacion)
+        VALUES          (1700, 'Lucas', 'Felix', '2022-11-23');
+
+INSERT INTO Experiencia (cuil, idTecnologia, calificacion)
+        VALUES                  (1700, 8, 0);
+
+INSERT INTO Tecnologia (idTecnologia, tecnologia, costoBase)
+        VALUES                  (8, 'Node.js', 19000);
+
+INSERT INTO Tarea (idRequerimiento, cuil, inicio, fin)
+        VALUES          (12, 1700, '2022-08-01', '2022-11-23');
+
+INSERT INTO Requerimiento (idRequerimiento, idProyecto, idTecnologia, descripcion, complejidad)
+        VALUES                  (21, 31, 8, 'Microservicio de Persistencia 2', 9);
+
+INSERT INTO Proyecto (idProyecto, cuit, descripcion, presupuesto, inicio, fin)
+        VALUES          (40, 400, 'Canino APP 2', 30000, '2022-08-01', '2022-11-23');
+
+INSERT INTO Cliente (cuit, razonSocial)
+        VALUES          (400, 'Unicornio Verde');
